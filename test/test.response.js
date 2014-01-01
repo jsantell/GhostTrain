3eccfd023372d4d5ae1b3c4979f3a8d7dd3a88c8
Jordan Santell <jsantell@gmail.com>
2013-12-31 16:39:05 -0800

    Shell for test.response.js

--- test/test.response.js
+++ test/test.response.js
@@ -3,4 +3,35 @@ var GhostTrain = require('..');
 var expect = require('chai').expect;
 
 describe('Response', function () {
+  describe('properties', function () {
+
+  });
+
+  describe('res.header(), res.set()', function () {
+
+  });
+  
+  describe('res.get()', function () {
+
+  });
+
+  describe('res.contentType(), res.type()', function () {
+
+  });
+
+  describe('res.status()', function () {
+
+  });
+
+  describe('res.json()', function () {
+
+  });
+  
+  describe('res.jsonp()', function () {
+
+  });
+
+  describe('res.send()', function () {
+
+  });
 });
